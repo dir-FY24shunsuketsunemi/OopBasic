500d65ba3f2aa8815243eb97d2e12f0a68d18a85
dir-FY24shunsuketsunemi <shnsuke.tsunemi.d372@dirbato.co.jp>
2024-04-10 09:11:34 +0900

    a

---
+++ OopBasic/src/company/ObjectSample1.java
@@ -0,0 +1,10 @@
+package company;
+
+public class ObjectSample1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ OopBasic/src/company/ObjectSmaple7.java
@@ -0,0 +1,10 @@
+package company;
+
+public class ObjectSmaple7 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
